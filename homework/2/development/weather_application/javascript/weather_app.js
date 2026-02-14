1184c2b88d3aa1d1976e4b8926f061a4bdfef165
logan <dweltbadger@proton.me>
2026-02-14 13:44:07 -0500

    Corrected (maid it async) the zip code input section

--- homework/2/development/weather_application/javascript/weather_app.js
+++ homework/2/development/weather_application/javascript/weather_app.js
@@ -1,49 +1,54 @@
+const api_key = Object.freeze("");
 
 
-function collect_input (text_box) {
-    // Gets the value from the text box, then convers that to a integer, and then returns it
+// Returns the numerical representation of the text in the text box
+function read_zip_code_text (text_box) {
     return parseInt(text_box.value);
 }
 
-function get_zip_code () {
-    let zip_code = 0;
-    const submission_button = document.getElementById("zip-code-submission-button");
-    const text_box = document.getElementById("zip-code-text-box");
-
-    // First check if we have both the text box and the button
-    if (text_box) {
-        if (submission_button) {
-            // Collect input, when either the submission button is clicked, or when the textbox has enter
-            submission_button.addEventListener('click', () => {
-                zip_code = collect_input(text_box);
-            });
-            text_box.addEventListener('keydown', (key) => {
-                if (key.key === 'Enter') {
-                    zip_code = collect_input(text_box);
-                }
-            });
-        }
-        else {
-            console.log("The zip code submission button: " + submission_button.id + " is not found");
-        }
-    }
-    else {
-        console.log("Text box, " + text_box.id + "has not been found");
-    }
-
-    return zip_code;
+function zip_code_entered (button, text_box) {
+    return new Promise ( (result) => {
+        button.addEventListener('click', () => {
+            result(read_zip_code_text(text_box));
+        });
+        text_box.addEventListener('keydown', (key) => {
+            if (key.key === 'Enter') {
+                result(read_zip_code_text(text_box));
+            }
+        });
+    });
 }
 
 
-function running_program () {
-    let zip_code = -1;
-    
-    zip_code = get_zip_code();
-    
+async function get_zip_code () {
+    const submission_button = document.getElementById('zip-code-submission-button');
+    const text_box = document.getElementById('zip-code-text-box');
+
+    // Check to see if it retrieved submission button and text box correctly
+    if (!submission_button) {
+        console.log("The submission button failed to retrieve");
+        return;
+    }
+    else if (!text_box) {
+        console.log("Failed to retrieve the zip code text box");
+        return;
+    }
+
+    // Otherwise, both were successfully obtained
+    return await zip_code_entered(submission_button, text_box);
+}
+
+async function start_app () {
+    let zip_code = -1
+    // Creation Section
+
+    // Action Section
+    zip_code = await get_zip_code();                        // Do I need the await hear, if I have the await right were I need it, down the chain
+    console.log(`Zip Code: ` + zip_code);
 }
 
 function main () {
-    running_program();
+    start_app();
 }   
 main();
 
