2ad19cac9abdecbc4834a75fd3703b3812dac328
logan <dweltbadger@proton.me>
2026-02-14 17:08:58 -0500

    Temporary commit

---
+++ homework/2/development/dog_api/javascript/dog_api.js
@@ -0,0 +1,16 @@
+
+
+
+function start_program () {
+    // let list_of_breeds;
+
+    // First connect to the api, and get all of the dog breeds
+    get_dog_breeds();
+
+    console.log("STOPPER");
+}
+
+function main () {
+    start_program();
+}
+main();
