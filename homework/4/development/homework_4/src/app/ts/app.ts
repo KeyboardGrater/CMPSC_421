e7cb9046fa422a90439bb0f058e4c2f85ac0cce2
logan <dweltbadger@proton.me>
2026-03-24 18:50:34 -0400

    Laid out the skeleton of the app states

--- homework/4/development/homework_4/src/app/ts/app.ts
+++ homework/4/development/homework_4/src/app/ts/app.ts
@@ -15,34 +15,50 @@ export class App {
 
   // Defined
   private appState: APP_STATE;
-  // private timeRemaining: TimerConfig;
-  private timeRemaining: number;
-  private timerId: ReturnType<typeof setInterval> | undefined; 
-  // private displayTime: string;
-  
+  private timeRemaining = signal(10);
+  private timerId: ReturnType<typeof setInterval> | undefined;
+
   constructor() {
-    // State 
-    this.appState = "before-run"
-
-    // Timer 
-    // this.timeRemaining = {hours: 0, minutes: 25, seconds: 0};
-    this.timeRemaining = 25 * 60;
-
-    // Display Time
-    // this.displayTime = "";
+    // State
+    this.appState = "before-run";
   }
 
   modifyTimer() {
     // First check what the current state of the app is, and then base the action off of that
     
     // Start the timer, if we have yet to run it
-    if (this.appState === "before-run") {
+    if (this.appState === "before-run") {      
       // Maybe move the timer up
-
+      
       // Start the timer
       this.startTimer();
       // Update the state of the app to running
       this.appState = "running";
+    }
+
+    // When the user presses the button when the program is running (counting down)
+    else if (this.appState === "running") {
+      // Stop the timer
+      clearInterval(this.timerId);
+
+      // Update the state of the app to paused
+      this.appState = "paused";
+
+    }
+
+    // When the user presses the button after it has been paused
+    else if (this.appState === "paused") {
+      // Restart the timer, but starting at the new timeRemaining amount
+      this.startTimer();
+
+      // Update the state of the app
+      this.appState = "running";
+    }
+
+    // When the timer ends up reaching zero (if this.appState === "after-running")
+    else {  
+      console.log("Reached the timer is up point");
+      // TODO, store task in list
     }
   }
 
@@ -54,24 +70,28 @@ export class App {
     // this.timerId = setInterval(this.displayTime, 1_000);
     this.timerId = setInterval( () => {
       // If the time has run out then close down the timer
-      if (this.timeRemaining <= 0) {
+      if (this.timeRemaining() <= 0) {
+        // Change the state of the app to after-running
+        this.appState = "after-running";
+        console.log(this.appState);
+        // Cancel the interval timer
         clearInterval(this.timerId);
       }
-
+      else {
       // Otherwise, keep decrementing the clock
       this.decrement_clock();
+      }
     }, 1_000);
-
   }
 
-  decrement_clock (): void {
-    this.timeRemaining--;
+  decrement_clock(): void {
+    this.timeRemaining.update(v => v - 1);
   }
 
   get displayTime(): string {
     let hours_num: number;
     let minutes_num: number;
-    let seconds_num: number = this.timeRemaining;
+    let seconds_num: number = this.timeRemaining();
     let hours_str: string;
     let minutes_str: string;
     let seconds_str: string;
@@ -86,7 +106,7 @@ export class App {
     // String handling
     hours_str = hours_num.toString().padEnd(2, '0');
     minutes_str = minutes_num.toString().padEnd(2, '0');
-    seconds_str = seconds_num.toString().padEnd(2, '0');
+    seconds_str = seconds_num.toString().padStart(2, '0'); // FIX THIS
 
     return `${hours_str}:${minutes_str}:${seconds_str}`;
   }
