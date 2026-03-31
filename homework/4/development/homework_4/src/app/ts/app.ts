5a12bcbae390f79770bba12b04079db184c76653
logan <dweltbadger@proton.me>
2026-03-31 16:06:00 -0400

    Added task input handling, and temporarily disabled the timer stuff

--- homework/4/development/homework_4/src/app/ts/app.ts
+++ homework/4/development/homework_4/src/app/ts/app.ts
@@ -1,64 +1,63 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import type { APP_STATE } from './interfaces_and_constants';
 import { APP_BASE_HREF } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: '../html/app.html',
-  styleUrl: '../css/app.css'
+  styleUrls: ['../css/app.css']
 })
 export class App {
   protected readonly title = signal('homework_4');
 
   // Defined
+  public task: string;
+  public taskObtained: boolean;
+  
   private appState: APP_STATE;
   private timeRemaining = signal(10);
   private timerId: ReturnType<typeof setInterval> | undefined;
-
+  private taskList: string [];
   constructor() {
     // State
     this.appState = "before-run";
+    this.task = "";
+    this.taskList = [];
+    this.taskObtained = false;
   }
 
   modifyTimer() {
     // First check what the current state of the app is, and then base the action off of that
-    
-    // Start the timer, if we have yet to run it
-    if (this.appState === "before-run") {      
-      // Maybe move the timer up
-      
-      // Start the timer
-      this.startTimer();
-      // Update the state of the app to running
-      this.appState = "running";
-    }
-
-    // When the user presses the button when the program is running (counting down)
-    else if (this.appState === "running") {
-      // Stop the timer
-      clearInterval(this.timerId);
-
-      // Update the state of the app to paused
-      this.appState = "paused";
-
-    }
-
-    // When the user presses the button after it has been paused
-    else if (this.appState === "paused") {
-      // Restart the timer, but starting at the new timeRemaining amount
-      this.startTimer();
-
-      // Update the state of the app
-      this.appState = "running";
-    }
-
-    // When the timer ends up reaching zero (if this.appState === "after-running")
-    else {  
-      console.log("Reached the timer is up point");
-      // TODO, store task in list
+    switch (this.appState) {
+      case "before-run":
+        // Start the timer, if we have yet to run it
+        this.startTimer();
+        // Update the state of the app to running
+        this.appState = "running";
+        break;
+      case "running":
+        // When the user presses the button when the program is running (counting down)
+        // Stop the timer
+        clearInterval(this.timerId);
+        // Update the state of the app to paused
+        this.appState = "paused";
+        break;
+      case "paused":
+        // When the user presses the button after it has been paused
+        // Restart the timer, but starting at the new timeRemaining amount
+        this.startTimer();
+        // Update the state of the app
+        this.appState = "running";
+        break;
+      default:        // After running
+        // When the timer ends up reaching zero (if this.appState === "after-running")
+        console.log("Reached the timer is up point");
+        // Store the task in the list
+        this.taskList.push(this.task);
     }
   }
 
@@ -106,8 +105,30 @@ export class App {
     // String handling
     hours_str = hours_num.toString().padEnd(2, '0');
     minutes_str = minutes_num.toString().padEnd(2, '0');
-    seconds_str = seconds_num.toString().padStart(2, '0'); // FIX THIS
+    seconds_str = seconds_num.toString().padStart(2, '0'); // TODO: FIX THIS
 
     return `${hours_str}:${minutes_str}:${seconds_str}`;
   }
+
+  // Task handling and related
+  onTaskInput(text: string) {
+
+    // Check to see if the check is not whitespace nor empty. if that is is the case, then don't do anything and return
+    if (text.trim() === "") {
+      return;
+    }
+
+    // Otherwise we can do the actions with text now
+    
+    // Set the task as the text
+    this.task = text;
+
+    // Update the obtainedTask flag to true
+    this.taskObtained = true;
+  }
+
+  onStartButtonClick() {
+    
+  }
+
 }
